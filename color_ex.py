f2de8edbed6e2482c668b502f07ac157e82c6bb2
Luis Jean <luisjeanknowsbest@gmail.com>
2022-10-31 17:45:06 -0400

    Update color_ex.py

--- color_ex.py
+++ color_ex.py
@@ -4,3 +4,10 @@ import sys
 import time
 import numpy as np
 from PIL import Image
+import imp
+import glob
+from sklearn.cluster import KMeans
+import skimage.filters as skf
+import skimage.color as skc
+import skimage.morphology as skm
+from skimage.measure import label
